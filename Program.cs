d19c960fb47218dd7bb08a5f9efde757b47bd44d
OlekanmaVictoria <olekanmavictoria1994@gmail.com>
2022-04-20 18:07:38 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+bool lessthan100(int a,int b){
+    if(a + b < 100)
+        return true;
+    else 
+        return false;
+
+}
+
+Console.WriteLine(lessthan100(22 , 15));
+Console.WriteLine(lessthan100(83 , 34));
+Console.WriteLine(lessthan100(3 , 77));
